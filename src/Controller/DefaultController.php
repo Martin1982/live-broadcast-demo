2afe303f6a8c5ae852ca0a24c926084e2471ce92
Martin de Keijzer <martin@spinninrecords.nl>
2019-06-24 16:44:33 +0200

    Update to the latest broadcast bundle version

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use Martin1982\LiveBroadcastBundle\Broadcaster\Scheduler;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController extends AbstractController
+{
+    protected $scheduler;
+
+    public function __construct(Scheduler $scheduler)
+    {
+        $this->scheduler = $scheduler;
+    }
+
+    /**
+     * @Route("/", name="homepage")
+     *
+     * @param Request $request
+     *
+     * @return Response
+     */
+    public function indexAction(Request $request): Response
+    {
+        $schedule = null;
+
+        if ($request->get('schedule') === '1') {
+            $this->scheduler->applySchedule();
+
+            $schedule = 'Running scheduler..';
+        }
+
+        return $this->render('default/index.html.twig', [
+            'schedule' => $schedule,
+        ]);
+    }
+}
