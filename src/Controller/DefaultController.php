ee69742bbf5ddda4acfdc045c3b2489409b6823c
Martin de Keijzer <martin@spinninrecords.nl>
2020-11-02 17:11:09 +0100

    Add basic terms, conditions and privacy policy to adhere by API platform conditions

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,10 +3,11 @@
 namespace App\Controller;
 
 use Martin1982\LiveBroadcastBundle\Broadcaster\Scheduler;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Martin1982\LiveBroadcastBundle\Exception\LiveBroadcastException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
@@ -23,6 +24,8 @@ class DefaultController extends AbstractController
      * @param Request $request
      *
      * @return Response
+     *
+     * @throws LiveBroadcastException
      */
     public function indexAction(Request $request): Response
     {
@@ -36,6 +39,42 @@ class DefaultController extends AbstractController
 
         return $this->render('default/index.html.twig', [
             'schedule' => $schedule,
+        ]);
+    }
+
+    /**
+     * @Route("/terms-of-use")
+     *
+     * Terms of use page
+     *
+     * @param Request $request
+     *
+     * @return Response
+     */
+    public function termsOfUseAction(Request $request): Response
+    {
+        return $this->render('default/terms_of_use.html.twig', [
+            'broadcast_app_name' => $this->getParameter('app.name'),
+            'broadcast_owner' => $this->getParameter('app.owner'),
+            'broadcast_url' => $request->getSchemeAndHttpHost(),
+        ]);
+    }
+
+    /**
+     * @Route("/privacy-policy")
+     *
+     * Privacy policy page
+     *
+     * @param Request $request
+     *
+     * @return Response
+     */
+    public function privacyPolicyAction(Request $request): Response
+    {
+        return $this->render('default/privacy_policy.html.twig', [
+            'broadcast_app_name' => $this->getParameter('app.name'),
+            'broadcast_owner' => $this->getParameter('app.owner'),
+            'broadcast_url' => $request->getSchemeAndHttpHost(),
         ]);
     }
 }
